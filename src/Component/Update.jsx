2ae946391563518e90137981a21d82ffdd9f2763
syma sultana <symasultana2002@gmail.com>
2025-03-23 21:06:03 +0600

    change the update using put operation

--- src/Component/Update.jsx
+++ src/Component/Update.jsx
@@ -1,29 +1,45 @@
 import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
-  const user = useLoaderData(); // Get data from loader
+  const loadedUser = useLoaderData(); // Get data from loader
 
-
-  
-  const handleUpdate = event => {
+  const handleUpdate = (event) => {
     event.preventDefault(); // Prevent default form submission
 
     const form = event.target;
     const name = form.name.value;
     const email = form.email.value;
-    const user = { name, email };
+    const updatedUser = { name, email };
 
-    console.log(user);
-  }
+    console.log(updatedUser);
+
+    fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Updated user:", data);
+        if(data.modifiedCount>0){
+            alert("user updated successfully")
+           
+          }
+      })
+      .catch((error) => console.error("Error updating user:", error));
+  };
+
   return (
     <div>
-      <h2>Update User: {user.name}</h2>
-      <form  onSubmit={handleUpdate}>
-        <input type="text" name="name"defaultValue={user?.name} required />
+      <h2>Update User: {loadedUser.name}</h2>
+      <form onSubmit={handleUpdate}>
+        <input type="text" name="name" defaultValue={loadedUser?.name} required />
         <br />
-        <input type="email" name="email" defaultValue={user?.email} required />
+        <input type="email" name="email" defaultValue={loadedUser?.email} required />
         <br />
-        <input type="submit" value="Add User" />
+        <input type="submit" value="Update User" />
       </form>
     </div>
   );
